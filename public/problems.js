cd46745506f30e1787b80ee9deac8da0c71c86bc
fahimmutashir23 <taslimit23@gmail.com>
2024-01-24 06:29:46 +0600

    finish Project

---
+++ public/problems.js
@@ -0,0 +1,12 @@
+const isPalindrome = (value) => {
+    const conLowCase = value.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reverseStr = conLowCase === conLowCase.split('').reverse().join('');
+    return reverseStr
+}
+
+const text1 = "level"
+const text2 = "hello"
+const text3 = "A man, a plan, a canal: Panama"
+
+
+console.log(isPalindrome(text1))
--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import useIsLogIn from "../../Hooks/useIsLogIn";
+import logo from "../../assets/Logo/Logo2.png"
 
 const Navbar = () => {
 const user = useIsLogIn()
@@ -13,8 +14,8 @@ const user = useIsLogIn()
       <div className="max-w-7xl flex flex-wrap items-center justify-between mx-auto py-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
+            src={logo}
+            className="w-16"
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
